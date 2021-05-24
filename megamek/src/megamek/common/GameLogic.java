072f8ce8c4046e95aa5f0e028900fe19f22c9e4e
Ebert Schoofs <ebertschoofs@gmail.com>
2021-05-24 20:10:04 +0200

    Split the responsibilities of GameLogic.updatePlayerScores into helper functionalities to reduce Cognitive Complexity

--- megamek/src/megamek/common/GameLogic.java
+++ megamek/src/megamek/common/GameLogic.java
@@ -243,55 +243,67 @@ public class GameLogic {
         return vr.victory();
     }
 
+    private HashedMap computeOpponentScores(boolean teamwin) {
+        HashedMap opponent_scores = new HashedMap();
+
+        for (Enumeration<IPlayer> i = game.getPlayers(); i.hasMoreElements(); ) {
+            IPlayer player = i.nextElement();
+            int opponent_score = 0;
+            for (Enumeration<IPlayer> j = game.getPlayers(); j.hasMoreElements(); ) {
+                IPlayer opponent = j.nextElement();
+                if (teamwin) {
+                    if (opponent.getTeam() != player.getTeam()) {
+                        opponent_score += opponent.getScore().getTotalScore();
+                    }
+                } else if (opponent != player) {
+                    opponent_score += opponent.getScore().getTotalScore();
+                }
+            }
+            opponent_scores.put(player.getId(), opponent_score);
+        }
+
+        return opponent_scores;
+    }
+
+    private void updatePlayerScores(HashedMap opponent_scores, int winner, boolean teamwin) {
+        for (Enumeration<IPlayer> i = game.getPlayers(); i.hasMoreElements(); ) {
+            IPlayer player = i.nextElement();
+            if (teamwin) {
+                if (winner == player.getTeam()) {
+                    player.getScore().win((Integer) opponent_scores.get(player.getId()));
+                } else {
+                    player.getScore().lose((Integer) opponent_scores.get(player.getId()));
+                }
+            }
+            else {
+                if (winner == player.getId()) {
+                    player.getScore().win((Integer) opponent_scores.get(player.getId()));
+                }
+                else {
+                    player.getScore().lose((Integer) opponent_scores.get(player.getId()));
+                }
+            }
+        }
+    }
+
+
     //TODO set to private once refactoring is done
     public void updatePlayerScores() {
         //Assure that the playerscores are only updated when in the victory phase.
         if (game.getPhase() == IGame.Phase.PHASE_VICTORY) {
-            int wonPlayer = game.getVictoryPlayerId();
             int wonTeam = game.getVictoryTeam();
 
-            HashedMap opponent_scores = new HashedMap();
+            boolean teamwin = wonTeam != IPlayer.TEAM_NONE;
+            HashedMap opponent_scores = computeOpponentScores(teamwin);
 
-            for (Enumeration<IPlayer> i = game.getPlayers(); i.hasMoreElements(); ) {
-                IPlayer player = i.nextElement();
-                int opponent_score = 0;
-                if (wonTeam != IPlayer.TEAM_NONE) {
-                    for (Enumeration<IPlayer> j = game.getPlayers(); j.hasMoreElements(); ) {
-                        IPlayer opponent = j.nextElement();
-                        if (opponent.getTeam() != player.getTeam()) {
-                             opponent_score += opponent.getScore().getTotalScore();
-                        }
-                    }
-                }
-                else if (wonPlayer != IPlayer.PLAYER_NONE) {
-                    for (Enumeration<IPlayer> j = game.getPlayers(); j.hasMoreElements(); ) {
-                        IPlayer opponent = j.nextElement();
-                        if (opponent != player) {
-                            opponent_score += opponent.getScore().getTotalScore();
-                        }
-                    }
-                }
-                opponent_scores.put(player.getId(), opponent_score);
+            if (teamwin) {
+                updatePlayerScores(opponent_scores, wonTeam, true);
             }
-            for (Enumeration<IPlayer> i = game.getPlayers(); i.hasMoreElements(); ) {
-                IPlayer player = i.nextElement();
-                if (wonTeam != IPlayer.TEAM_NONE) {
-                    if (wonTeam == player.getTeam()) {
-                        player.getScore().win((Integer) opponent_scores.get(player.getId()));
-                    }
-                    else {
-                        player.getScore().lose((Integer) opponent_scores.get(player.getId()));
-                    }
-                }
-                else if (wonPlayer != IPlayer.PLAYER_NONE) {
-                    if (wonPlayer == player.getId()) {
-                        player.getScore().win((Integer) opponent_scores.get(player.getId()));
-                    }
-                    else {
-                        player.getScore().lose((Integer) opponent_scores.get(player.getId()));
-                    }
-                }
+            else {
+                int wonPlayer = game.getVictoryPlayerId();
+                updatePlayerScores(opponent_scores, wonPlayer, false);
             }
+
         }
     }
 
